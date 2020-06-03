aa3bb24b5a4cd306dec5b15070ec6551910d93b4
Christian <chsanleo@gmail.com>
2020-06-03 23:39:11 +0200

    Feature Choose view
    Add changes in the view part when the choose feature work, show it to user

--- js/game.js
+++ js/game.js
@@ -1,48 +1,57 @@
-let game ={
+let game = {
 
-    choose(fighterID){
-        let fighterChoosedPosition = Number(fighterID)-1;
+    choose(fighterID) {
+        if (partida.teamPlayer2.length == MAXNUMTEAMFIGHTERS){return;}
+        let fighterChoosedPosition = Number(fighterID) - 1;
 
-        if(partida.teamPlayer1.length == EMPTY || partida.teamPlayer2.length == partida.teamPlayer1.length){
+        if (partida.teamPlayer1.length == EMPTY || partida.teamPlayer2.length == partida.teamPlayer1.length) {
             partida.teamPlayer1.push(allFighters[fighterChoosedPosition]);
-
-            game.disableFighter(fighterID);   
-            game.updateTeam(1);
+            
+            let currentTeam = 1;
+            game.disableFighter(fighterID);
+            game.updateTeam(currentTeam);
+            game.changeTextPlayer(currentTeam,2);
             return;
         }
-
+        let currentTeam = 2;
         partida.teamPlayer2.push(allFighters[fighterChoosedPosition]);
         game.disableFighter(fighterID);
-        game.updateTeam(2);
-
-        if(partida.teamPlayer2.length == MAXNUMTEAMFIGHTERS){ nextStage();}
+        game.updateTeam(currentTeam);
+        game.changeTextPlayer(currentTeam,1);
+        if (partida.teamPlayer2.length == MAXNUMTEAMFIGHTERS) { nextStage(); }
     },
 
-    disableFighter(fighterID)
-    {
+    disableFighter(fighterID) {
         let fighter = document.getElementById(`fighter${fighterID}`);
-        fighter.setAttribute(`onclick`,``); 
+        fighter.setAttribute(`onclick`, ``);
+        let classFighter = fighter.getAttribute('class');
+        classFighter += ' disable';
+        fighter.setAttribute('class', classFighter);
     },
 
-    updateTeam(team){
+    updateTeam(team) {
         let player = document.getElementById(`chossedPlayer${team}`);
-        player.innerHTML ='';
+        player.innerHTML = '';
         let teamList = partida.teamPlayer2;
 
-        if(team == Number(1)){teamList = partida.teamPlayer1; }
+        if (team == Number(1)) { teamList = partida.teamPlayer1; }
 
-        for(const person of teamList){
-          let luchador = document.createElement('img');
-          let indexLuchador = allNameFighters.indexOf(person.nombre) + 1;
-          luchador.setAttribute("src",`img/human${indexLuchador}.jpg`);
-          luchador.setAttribute("class","luchador");
-          //luchador.setAttribute("title", ` Name: ${person.nombre} Attack: ${person.ataque} Defense: ${person.defensa} Lucky: ${person.suerte}`);
-          
-          player.appendChild(luchador);
+        for (const person of teamList) {
+            let luchador = document.createElement('img');
+            let indexLuchador = allNameFighters.indexOf(person.nombre) + 1;
+            luchador.setAttribute("src", `img/human${indexLuchador}.jpg`);
+            luchador.setAttribute("class", "luchador");
+
+            player.appendChild(luchador);
         };
-      },
+    },
 
-    nextStage(){
+    changeTextPlayer(oldNumber, newNumber) {
+        let playerChooseText = document.getElementById('chooseText');
+        playerChooseText.innerHTML = playerChooseText.outerHTML.replace(oldNumber,newNumber);
+    },
+
+    nextStage() {
 
 
     }
